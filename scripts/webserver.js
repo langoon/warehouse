7c3b8511d9b1783e04249a5f7fa8b7d02394f1fe
Stefan <stefan.astrand@unitar.io>
2020-07-17 14:53:27 +0200

    Code formatting

--- scripts/webserver.js
+++ scripts/webserver.js
@@ -9,18 +9,18 @@ createServer((req, res) => {
   }
   else if (req.method === "POST") {
     try {
-    console.log(`New code has been deployed – pull and install`)
-    console.log(`Reset any changes that have been made locally`)
-    console.log(execSync(`git -C ${process.cwd()} reset --hard`))
-    console.log(`Ditch any files that have been added locally too`)
-    console.log(execSync(`git -C ${process.cwd()} clean -df`))
-    console.log(`Pull down the latest`)
-    console.log(execSync(`git -C ${process.cwd()} pull -f`))
-    console.log(`Install requirements`)
-    console.log(execSync(`pip3 install -r requirements.txt`))
-    console.log(`All good`)
-    res.statusCode = 200
-    return res.end("Code was pulled repo")
+      console.log(`New code has been deployed – pull and install`)
+      console.log(`Reset any changes that have been made locally`)
+      console.log(execSync(`git -C ${process.cwd()} reset --hard`))
+      console.log(`Ditch any files that have been added locally too`)
+      console.log(execSync(`git -C ${process.cwd()} clean -df`))
+      console.log(`Pull down the latest`)
+      console.log(execSync(`git -C ${process.cwd()} pull -f`))
+      console.log(`Install requirements`)
+      console.log(execSync(`pip3 install -r requirements.txt`))
+      console.log(`All good`)
+      res.statusCode = 200
+      return res.end("Code was pulled repo")
     } catch (error) {
       console.log(`Could not pull from repo, failed with exception:`)
       console.log(error)
